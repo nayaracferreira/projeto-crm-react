61c262e49fd558bf896d30b55f564b68b6cab26e
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-08-22 22:32:52 -0300

    Update AppHeader.jsx

--- Documents/projeto_crm_react/src/components/organisms/AppHeader.jsx
+++ Documents/projeto_crm_react/src/components/organisms/AppHeader.jsx
@@ -4,7 +4,7 @@ export default function AppHeader() {
   return (
     <header>        
         <nav>
-            <h1><a href="/" aria-label="Tera Agendaaaaa: Encaminha para a página inicial">Tera Agenda</a></h1>                
+            <h1><a href="/" aria-label="Tera Agenda: Encaminha para a página inicial">Tera Agenda</a></h1>                
         </nav>
     </header>
   )
