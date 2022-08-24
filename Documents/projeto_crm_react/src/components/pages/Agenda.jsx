29c9714bf5f8041ce441ca7dafb62ca282f8e7a5
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-08-24 10:29:51 -0300

    Update Agenda.jsx

--- Documents/projeto_crm_react/src/components/pages/Agenda.jsx
+++ Documents/projeto_crm_react/src/components/pages/Agenda.jsx
@@ -7,7 +7,7 @@ import { AppFooter, AppHeader } from '../organisms';
 
 export default function Agenda() {
   return (
-    <body>    
+    <div className="body">    
     
     <AppHeader/>
 
@@ -18,6 +18,6 @@ export default function Agenda() {
 
    <AppFooter/>
    
-</body>
+</div>
   )
 }
