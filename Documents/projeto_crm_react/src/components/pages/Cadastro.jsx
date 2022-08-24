bca16bd67f86527194a8181864ef1a3b670183dc
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-08-24 10:24:35 -0300

    Update Cadastro.jsx

--- Documents/projeto_crm_react/src/components/pages/Cadastro.jsx
+++ Documents/projeto_crm_react/src/components/pages/Cadastro.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-import { AppFooter, AppHeader, } from '../organisms';
+import { AppFooter, AppHeader, Cep } from '../organisms';
 
 export default function Cadastro() {
   return (
-    <body>
+  
+    <div className="body">
     
     <AppHeader/>  
  
     <main> 
 
-        <form action="/login" id="formCadastre" className="size-margin-90-5 gapColuna" onsubmit=" funcConfirmar(event)">
+        <form action="/login" id="formCadastre" className="size-margin-90-5 gapColuna" onSubmit=" funcConfirmar(event)">
             <h2 className="textShadowTitles">Cadastre suas informações</h2>
         <div>
             <label htmlFor="razaoSocial">Razão Social*</label>
@@ -28,55 +29,20 @@ export default function Cadastro() {
         </div>
        
         <div>
-            <label fhtmlFor="email">E-mail*</label>
+            <label htmlFor="email">E-mail*</label>
             <input name="email" type="email" id="email" placeholder="...@email.com" required />
         </div>
 
-        <div className="local gapColuna">     
-            <div>
-                <label htmlFor="cep">Cep*</label>
-                <input name="cep" type="text" id="cep" placeholder="Ex:012345-678" required />
-            </div>
-
-            <div>
-                <label htmlFor="logradouro">Endereço*</label>
-                <input name="logradouro" type="text" id="logradouro" placeholder="Ex:Rua Vinte e Cindo de ..." />
-            </div>
-
-            <div>
-                <label htmlFor="numero">Número*</label>
-                <input name="numero" type="text" id="numero" placeholder="Ex:123" required />
-            </div>
-
-            <div>
-                <label htmlFor="blocoSala">Bloco/Sala</label>
-                <input name="blocoSala" type="text" id="blocoSala" placeholder="Número do bloco ou sala" />
-            </div>
-
-            <div>
-                <label htmlFor="bairro">Bairro*</label>
-                <input name="bairro" type="text" id="bairro" placeholder="Informe o bairro" />
-            </div>
-
-            <div>
-                <label htmlFor="localidade">Cidade*</label>
-                <input name="localidade" type="text" id="localidade" placeholder="Informe a cidade" />
-            </div>
-     
-            <div>
-                <label htmlFor="uf">UF*</label>
-                <input name="uf" type="text" id="uf" placeholder="Informe a UF" />                
-            </div>    
-        </div>
+        <Cep/>
                
         <div>
             <label htmlFor="cadastroSenha">Senha*</label>
-            <input name="cadastroSenha" type="password" id="cadastroSenha" placeholder="Informe a senha" minlength="8" maxlength="8" required />
+            <input name="cadastroSenha" type="password" id="cadastroSenha" placeholder="Informe a senha" minLength="8" maxLength="8" required />
         </div>
 
         <div>
             <label htmlFor="confirmSenha">Confirme a senha*</label>
-            <input name="confirmSenha" type="password" id="confirmSenha" placeholder="Confirme a senha" minlength="8" maxlength="8" required />
+            <input name="confirmSenha" type="password" id="confirmSenha" placeholder="Confirme a senha" minLength="8" maxLength="8" required />
         </div>
 
         <h3 className="textShadowTitles">Responsável pela empresa</h3>
@@ -105,7 +71,7 @@ export default function Cadastro() {
 <AppFooter/>
 
     
-</body>
+</div>
 
   )
 }
