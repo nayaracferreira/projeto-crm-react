2aca669cd62a947bd3f910159f3b015919e745a0
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-08-25 13:56:41 -0300

    Adicionando Imask e Cep

--- Documents/projeto_crm_react/src/components/organisms/index.jsx
+++ Documents/projeto_crm_react/src/components/organisms/index.jsx
@@ -2,5 +2,8 @@ import AppHeader from "./AppHeader";
 import AppHeaderHome from "./AppHeaderHome";
 import AppHeaderAgendamento from "./AppHeaderAgendamento";
 import AppFooter from "./AppFooter";
+import Cep from "./Cep";
+import Imask from "./Imask";
+import ImaskCnpj from "./ImaskCnpj";
 
-export {AppHeader, AppHeaderHome, AppHeaderAgendamento, AppFooter,};
+export {AppHeader, AppHeaderHome, AppHeaderAgendamento, Cep, Imask, ImaskCnpj, AppFooter};
